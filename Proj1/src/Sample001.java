8dd7fdd5eee203a52ebc583c80acc85c9ef0786c
HY <HY@192.168.1.9>
2019-05-02 10:39:25 +0900

    first commit

---
+++ Proj1/src/Sample001.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author HY
+ *
+ */
+public class Sample001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i=1;i<=5;i++) {
+			System.out.println("---");
+			System.out.println(i);
+		}
+	}
+
+}
